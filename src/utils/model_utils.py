3959da6ff5b7a67b0acbaac982c3197f0a0d2e61
Tanny Chavez <tanchavez@lbl.gov>
2024-11-18 13:19:59 +0800

    Remove content registry and add local json file

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -1,43 +1,19 @@
-import os
-
-import requests
-
-CONTENT_URL = str(os.environ["MLEX_CONTENT_URL"])
+import json
 
 
-def get_model_list():
-    """
-    Get a list of algorithms from content registry
-    """
-    models = []
-    response = requests.get(f"{CONTENT_URL}/models")
-    if response.status_code == 200:
-        for item in response.json():
-            if "data_clinic" in item["application"]:
-                models.append({"label": item["name"], "value": item["content_id"]})
-    else:
-        models.append({"label": "No models available", "value": ""})
-    return models
+class Models:
+    def __init__(self, modelfile_path="./assets/models.json"):
+        self.path = modelfile_path
+        f = open(self.path)
+        contents = json.load(f)["contents"]
+        self.modelname_list = [content["name"] for content in contents]
+        self.models = {}
 
+        for i, n in enumerate(self.modelname_list):
+            self.models[n] = contents[i]
 
-def get_gui_components(model_uid, comp_group):
-    """
-    Returns the GUI components of the corresponding model and action
-    Args:
-        model_uid:      Model UID
-        comp_group:     Action, e.g. training, testing, etc
-    Returns:
-        params:         List of model parameters
-    """
-    response = requests.get(
-        f"{CONTENT_URL}/models/{model_uid}/model/{comp_group}/gui_params"
-    )
-    return response.json()
-
-
-def get_model_content(content_id):
-    """
-    Get the model content: uri and commands
-    """
-    response = requests.get(f"{CONTENT_URL}/contents/{content_id}/content").json()
-    return response["uri"], response["cmd"]
+    def __getitem__(self, key):
+        try:
+            return self.models[key]
+        except KeyError:
+            raise KeyError(f"A model with name {key} does not exist.")
