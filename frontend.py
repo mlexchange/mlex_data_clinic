016bb72b7b91e2e7015f1fcb499f5d858eb6c714
Tanny Chavez <tanchavez@lbl.gov>
2024-11-20 16:40:26 +0800

    Update run training jobs with prefect

--- frontend.py
+++ frontend.py
@@ -1,17 +1,14 @@
 import os
 import pathlib
-import pickle
 import shutil
 import tempfile
 from uuid import uuid4
 
 from dash import Input, Output, State, dcc, html
 from dotenv import load_dotenv
-from file_manager.data_project import DataProject
 
 from src.app_layout import (
     DATA_DIR,
-    TILED_KEY,
     USER,
     app,
     long_callback_manager,
@@ -28,8 +25,7 @@ from src.callbacks.display import (  # noqa: F401
     update_slider_boundaries_prediction,
 )
 from src.callbacks.download import disable_download, toggle_storage_modal  # noqa: F401
-from src.callbacks.execute import close_resources_popup, execute  # noqa: F401
-from src.utils.data_utils import get_input_params, prepare_directories
+from src.callbacks.execute import run_train  # noqa: F401
 
 load_dotenv(".env")
 
@@ -83,135 +79,6 @@ def save_results(download, job_data, row):
         return dcc.send_file(f"{archive_path}.zip")
     else:
         return None
-
-
-@app.long_callback(
-    Output("job-alert-confirm", "is_open"),
-    Input("submit", "n_clicks"),
-    State("app-parameters", "children"),
-    State("num-cpus", "value"),
-    State("num-gpus", "value"),
-    State("action", "value"),
-    State("jobs-table", "data"),
-    State("jobs-table", "selected_rows"),
-    State({"base_id": "file-manager", "name": "data-project-dict"}, "data"),
-    State("model-name", "value"),
-    State("model-selection", "value"),
-    State("log-transform", "value"),
-    State("min-max-percentile", "value"),
-    running=[(Output("job-alert", "is_open"), "True", "False")],
-    manager=long_callback_manager,
-    prevent_initial_call=True,
-)
-def submit_ml_job(
-    submit,
-    children,
-    num_cpus,
-    num_gpus,
-    action_selection,
-    job_data,
-    row,
-    data_project_dict,
-    model_name,
-    model_id,
-    log,
-    percentiles,
-):
-    """
-    This callback submits a job request to the compute service according to the selected action & model
-    Args:
-        execute:            Execute button
-        submit:             Submit button
-        children:           Model parameters
-        num_cpus:           Number of CPUs assigned to job
-        num_gpus:           Number of GPUs assigned to job
-        action_selection:   Action selected
-        job_data:           Lists of jobs
-        row:                Selected row (job)
-        data_project_dict:  Data project information
-        model_name:         Model name/description assigned by the user
-        model_id:           UID of model in content registry
-        log:                Log toggle
-        percentiles:        Min-Max Percentile values
-    Returns:
-        open the alert indicating that the job was submitted
-    """
-    data_project = DataProject.from_dict(data_project_dict, api_key=TILED_KEY)
-    # Update retrieval of model_uri, train_cmd, prediction_cmd, tune_cmd
-    train_cmd, prediction_cmd, tune_cmd = 2, 3, 4
-    experiment_id, orig_out_path, data_info = prepare_directories(
-        USER,
-        data_project,
-        train=(action_selection != "prediction_model"),
-        correct_path=(DATA_DIR == DIR_MOUNT),
-    )  # noqa: F841
-    input_params = get_input_params(children)
-    input_params["log"] = log
-    input_params["percentiles"] = percentiles
-    kwargs = {}  # noqa: F841
-
-    # Find the relative data directory in docker container
-    if DIR_MOUNT == DATA_DIR:
-        relative_data_dir = "/app/work/data"
-        out_path = "/app/work/data" + str(orig_out_path).split(DATA_DIR, 1)[-1]
-        data_info = "/app/work/data" + str(data_info).split(DATA_DIR, 1)[-1]
-    else:
-        relative_data_dir = DATA_DIR
-
-    if action_selection == "train_model":
-        command = f"{train_cmd} -d {data_info} -o {out_path}"  # noqa: F841
-
-    elif action_selection == "tune_model":
-        training_exp_id = job_data[row[0]]["experiment_id"]
-        model_path = pathlib.Path(
-            f"{relative_data_dir}/mlex_store/{USER}/{training_exp_id}"
-        )
-        kwargs = {"train_params": job_data[row[0]]["parameters"]}  # noqa: F841
-        train_params = {}
-
-        # Get target size from training process
-        input_params["target_width"] = train_params["target_width"]
-        input_params["target_height"] = train_params["target_height"]
-
-        # Define command to run
-        command = (
-            f"{tune_cmd} -d {data_info} -m {model_path} -o {out_path}"  # noqa: F841
-        )
-
-    else:
-        training_exp_id = job_data[row[0]]["experiment_id"]
-        model_path = pathlib.Path(
-            f"{relative_data_dir}/mlex_store/{USER}/{training_exp_id}"
-        )
-        if job_data[row[0]]["job_type"] == "train_model":
-            train_params = job_data[row[0]]["parameters"]
-        else:
-            train_params = job_data[row[0]]["parameters"].split("Training Parameters:")[
-                -1
-            ]
-
-        kwargs = {"train_params": train_params}  # noqa: F841
-        train_params = {}
-
-        # Get target size from training process
-        input_params["target_width"] = train_params["target_width"]
-        input_params["target_height"] = train_params["target_height"]
-
-        # Define command to run
-        command = f"{prediction_cmd} -d {data_info} -m {model_path} -o {out_path}"  # noqa: F841
-
-        # Save data project dict
-        data_project_dict = data_project.to_dict()
-
-        with open(f"{orig_out_path}/.file_manager_vars.pkl", "wb") as file:
-            pickle.dump(
-                data_project_dict,
-                file,
-            )
-
-    job = 1  # Update job submission
-    job.submit(USER, num_cpus, num_gpus)
-    return True
 
 
 if __name__ == "__main__":
--- src/callbacks/execute.py
+++ src/callbacks/execute.py
@@ -1,47 +1,120 @@
-from dash import Input, Output, State, callback
+import os
+import traceback
+import uuid
+from datetime import datetime
+
+import pytz
+from dash import MATCH, Input, Output, State, callback
+from file_manager.data_project import DataProject
+from mlex_utils.prefect_utils.core import schedule_prefect_flow
+
+from src.utils.job_utils import parse_model_params, parse_train_job_params
+from src.utils.plot_utils import generate_notification
+
+MODE = os.getenv("MODE", "")
+TIMEZONE = os.getenv("TIMEZONE", "US/Pacific")
+FLOW_NAME = os.getenv("FLOW_NAME", "")
+PREFECT_TAGS = os.getenv("PREFECT_TAGS", ["data-clinic"])
 
 
 @callback(
-    Output("resources-setup", "is_open"),
-    Output("warning-cause", "data"),
-    Input("execute", "n_clicks"),
-    State("action", "value"),
-    State("jobs-table", "data"),
-    State("jobs-table", "selected_rows"),
-    State({"base_id": "file-manager", "name": "total-num-data-points"}, "data"),
+    Output(
+        {
+            "component": "DbcJobManagerAIO",
+            "subcomponent": "notifications_container",
+            "aio_id": MATCH,
+        },
+        "children",
+        allow_duplicate=True,
+    ),
+    Input(
+        {
+            "component": "DbcJobManagerAIO",
+            "subcomponent": "train_button",
+            "aio_id": MATCH,
+        },
+        "n_clicks",
+    ),
+    State("model-parameters", "children"),
+    State({"base_id": "file-manager", "name": "data-project-dict"}, "data"),
+    State("model-list", "value"),
+    State("log-transform", "value"),
+    State("min-max-percentile", "value"),
+    State(
+        {"component": "DbcJobManagerAIO", "subcomponent": "job_name", "aio_id": MATCH},
+        "value",
+    ),
     prevent_initial_call=True,
 )
-def execute(execute, action_selection, job_data, row, num_imgs):
+def run_train(
+    n_clicks,
+    model_parameter_container,
+    data_project_dict,
+    model_name,
+    log,
+    percentiles,
+    job_name,
+):
     """
-    This callback validates the ml model and opens the resources modal
+    This callback submits a job request to the compute service according to the selected action & model
     Args:
-        execute:            Execute button
-        action_selection:   Action selected
-        job_data:           Lists of jobs
-        row:                Selected row (job)
-        num_imgs:           Number of images
+        n_clicks:                   Number of clicks
+        model_parameter_container:  App parameters
+        data_project_dict:          Data project dictionary
+        model_name:                 Selected model name
+        log:                        Log transform
+        percentiles:                Min-max percentiles
+        job_name:                   Job name
     Returns:
-        open/close the resources setup modal, and submits the training/prediction job accordingly
-        warning_cause:      Activates a warning pop-up window if needed
+        open the alert indicating that the job was submitted
     """
-    if num_imgs == 0:
-        return False, "no_dataset"
-    elif action_selection != "train_model" and not row:
-        return False, "no_row_selected"
-    elif row:
-        if (
-            action_selection != "train_model"
-            and job_data[row[0]]["job_type"] == "prediction_model"
-        ):
-            return False, "no_row_selected"
-    return True, ""
+    model_parameters, parameter_errors = parse_model_params(
+        model_parameter_container, log, percentiles
+    )
+    # Check if the model parameters are valid
+    if parameter_errors:
+        notification = generate_notification(
+            "Model Parameters",
+            "red",
+            "fluent-mdl2:machine-learning",
+            "Model parameters are not valid!",
+        )
+        return notification
 
+    data_project = DataProject.from_dict(data_project_dict)
+    train_params, project_name = parse_train_job_params(
+        data_project, model_parameters, model_name
+    )
 
-@callback(
-    Output("resources-setup", "is_open", allow_duplicate=True),
-    Output("warning-cause", "data", allow_duplicate=True),
-    Input("submit", "n_clicks"),
-    prevent_initial_call=True,
-)
-def close_resources_popup(submit):
-    return False, ""
+    if MODE == "dev":
+        job_uid = str(uuid.uuid4())
+        job_message = (
+            f"Dev Mode: Job has been succesfully submitted with uid: {job_uid}"
+        )
+        notification_color = "primary"
+    else:
+        try:
+            # Schedule job
+            current_time = datetime.now(pytz.timezone(TIMEZONE)).strftime(
+                "%Y/%m/%d %H:%M:%S"
+            )
+            job_uid = schedule_prefect_flow(
+                FLOW_NAME,
+                parameters=train_params,
+                flow_run_name=f"{job_name} {current_time}",
+                tags=PREFECT_TAGS + ["train", project_name],
+            )
+            job_message = f"Job has been succesfully submitted with uid: {job_uid}"
+            notification_color = "indigo"
+        except Exception as e:
+            # Print the traceback to the console
+            traceback.print_exc()
+            job_uid = None
+            job_message = f"Job presented error: {e}"
+            notification_color = "danger"
+
+    notification = generate_notification(
+        "Job Submission", notification_color, "formkit:submit", job_message
+    )
+
+    return notification
