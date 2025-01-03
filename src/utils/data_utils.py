1b069b5d61a9f7916f357f44c581383d239d73bf
Tanny Chavez <tanchavez@lbl.gov>
2025-01-03 11:46:36 -0800

    Add tiled dataset

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -1,20 +1,70 @@
 import hashlib
+import os
 import pathlib
 import uuid
 
+import httpx
 import pandas as pd
+from humanhash import humanize
+from tiled.client import from_uri
 
 from src.app_layout import DATA_DIR, TILED_KEY
+
+RESULTS_TILED_URI = os.getenv("RESULTS_TILED_URI", "")
+RESULTS_TILED_API_KEY = os.getenv("RESULTS_TILED_API_KEY", "")
+
+
+class TiledDataLoader:
+    def __init__(self, data_tiled_uri, data_tiled_api_key):
+        self.data_tiled_uri = data_tiled_uri
+        self.data_tiled_api_key = data_tiled_api_key
+        self.data_client = from_uri(
+            self.data_tiled_uri,
+            api_key=self.data_tiled_api_key,
+            timeout=httpx.Timeout(30.0),
+        )
+
+    def refresh_data_client(self):
+        self.data_client = from_uri(
+            self.data_tiled_uri,
+            api_key=self.data_tiled_api_key,
+            timeout=httpx.Timeout(30.0),
+        )
+
+    def prepare_project_container(self, user, project_name):
+        """
+        Prepare a project container in the data store
+        """
+        last_container = self.data_client
+        for part in [user, project_name]:
+            if part in last_container.keys():
+                last_container = last_container[part]
+            else:
+                last_container = last_container.create_container(key=part)
+        return last_container
+
+    def get_data_by_trimmed_uri(self, trimmed_uri, slice=None):
+        """
+        Retrieve data by a trimmed uri (not containing the base uri) and slice id
+        """
+        if slice is None:
+            return self.data_client[trimmed_uri]
+        else:
+            return self.data_client[trimmed_uri][slice]
+
+
+tiled_results = TiledDataLoader(
+    data_tiled_uri=RESULTS_TILED_URI, data_tiled_api_key=RESULTS_TILED_API_KEY
+)
 
 
 def hash_list_of_strings(strings_list):
     """
     Produces a hash of a list of strings.
     """
-    concatenated_string = "".join(strings_list)
-    hash_object = hashlib.sha256()
-    hash_object.update(concatenated_string.encode("utf-8"))
-    return hash_object.hexdigest()
+    concatenated = "".join(strings_list)
+    digest = hashlib.sha256(concatenated.encode("utf-8")).hexdigest()
+    return humanize(digest)
 
 
 def prepare_directories(user_id, data_project, train=True, correct_path=False):
