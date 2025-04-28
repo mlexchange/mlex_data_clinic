18c09c0f8ceb0b177cd2af45e8d8108b2eecc4f4
Xiaoya Chong <150726549+xiaoyachong@users.noreply.github.com>
2025-04-27 19:44:07 -0700

    add mlflow build-in auth

--- src/utils/mlflow_utils.py
+++ src/utils/mlflow_utils.py
@@ -5,6 +5,8 @@ import mlflow
 from mlflow.tracking import MlflowClient
 
 MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
+MLFLOW_TRACKING_USERNAME = os.getenv("MLFLOW_TRACKING_USERNAME", "")
+MLFLOW_TRACKING_PASSWORD = os.getenv("MLFLOW_TRACKING_PASSWORD", "")
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +19,8 @@ def check_mlflow_ready():
         Exception: If MLflow is not reachable
     """
     try:
+        os.environ['MLFLOW_TRACKING_USERNAME'] = MLFLOW_TRACKING_USERNAME
+        os.environ['MLFLOW_TRACKING_PASSWORD'] = MLFLOW_TRACKING_PASSWORD
         mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
         client = MlflowClient()  # noqa: F841
         return True
